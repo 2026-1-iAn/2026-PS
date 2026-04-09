9b310ee52885a2df420683f27bd83d7d4a04662b
2026-1-iAn <20261ctb0100036@estudantes.ifpr.edu.br>
2026-04-09 20:04:48 +0000

    Aula 10 - 1003: soma simples

---
+++ beecrowd/1003-soma-simples.py
@@ -0,0 +1,22 @@
+'''
+Problema: beecrowd | 1003
+Data: 2026.04.09
+Estudante: Ian Forbeck
+'''
+
+# Objetivo: ???
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: ???
+# Processamento: ???
+# Saída: ???
+
+# Leitura das variáveis (observe o enunciado - quantas entradas há?)
+A = int(input())
+B = int(input())
+
+# Cálculo
+SOMA = A + B
+
+# Saída - observe o formato exato no enunciado
+print(f"SOMA = {A+B}")
