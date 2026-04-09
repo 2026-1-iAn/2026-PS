4a678504ed1490edeb8b4e458f99cd93768ecee0
2026-1-iAn <20261ctb0100036@estudantes.ifpr.edu.br>
2026-04-09 20:13:02 +0000

    Aula 10 - 1004: produto simples

---
+++ beecrowd/1004-produto-simples.py
@@ -0,0 +1,20 @@
+'''
+Problema: beecrowd | 1004
+Data:2026.04.09
+Estudante: Ian Forbeck
+'''
+#Objetivo: 
+
+# --- ANÁLISE (LIAC) ---
+# Entrada:
+# Processsamento:
+# Saída:
+
+A = int(input())
+B = int(input())
+
+# Qual operação o enunciado pede?
+PROD = A * B
+
+# Saída - observe o formato exato no enunciado
+print(f"PROD = {A*B}")
