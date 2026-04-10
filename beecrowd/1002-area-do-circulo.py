44332ef84fe4ce8f7c8471670df8b3e56b372561
2026-1-iAn <20261ctb0100036@estudantes.ifpr.edu.br>
2026-04-10 14:18:37 +0000

    Aula 10 - 1002: area do circulo

---
+++ beecrowd/1002-area-do-circulo.py
@@ -0,0 +1,24 @@
+'''
+Problema: beecrowd | 1002
+Data:2026.04.10
+Estudante: Ian Forbeck
+'''
+
+#Objetivo: Calcular a área de um círculo e exibir com até 4 casas decimais
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: um valor de ponto flutuante
+# Processsamento: calcular a área usando AREA = pi*R**2
+# Saída: a mensagem "A=" seguida pelo valor da área com até 4 casas após a vírgula
+
+# Leitura do raio como número decimal
+R = float(input())
+
+# Defina pi onforme enunciado indica
+pi = 3.14159
+
+# Qual é a fórmula da área do círculo?
+AREA = pi*R**2
+
+# Saída - observe o formato exato e o número de casas decimais no enunciado
+print(f"A={AREA: .4f}")
