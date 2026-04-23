7fa742a564af90dc18c3e2e27df06a31e8f11c37
2026-1-iAn <20261ctb0100036@estudantes.ifpr.edu.br>
2026-04-23 20:33:52 +0000

    Aula 13 - 1017 e 1012

---
+++ beecrowd/1016-distancia.py
@@ -0,0 +1,18 @@
+'''
+Problema: beecrowd | 1016
+Data: 2026.04.23
+Estudante: Ian Forbeck
+'''
+
+# Objetivo: 
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: 
+# Processamento: 
+# Saída:
+
+X = int(input())
+# Vai receber o valor de quilômetros a serem distanciados
+
+print(f"{X*2} minutos")
+# Mostra o valor de min que demora a se distanciarem
