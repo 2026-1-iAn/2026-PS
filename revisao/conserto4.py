6d4cc9035747345e327b1aefb6b5bbc5202908a6
2026-1-iAn <20261ctb0100036@estudantes.ifpr.edu.br>
2026-09-18 21:56:19 +0000

    RevisaoPython

---
+++ revisao/conserto4.py
@@ -0,0 +1,7 @@
+# Conserto 4: trecho do "Pedra-Papel-Tesoura" (Aula 17)
+jogada = input("pedra, papel ou tesoura? ").lower()
+
+if jogada == "pedra" or jogada == "papel" or jogada == "tesoura":
+    print("Jogada valida:", jogada)
+else:
+    print("Jogada invalida!")
---
+++ revisao/conserto6.py
@@ -0,0 +1,4 @@
+# Conserto 6: menu de escolhas
+jogos = ["Adivinhe o Numero", "Pedra-Papel-Tesoura", "Par ou Impar"]
+opcao = int(input("Escolha o jogo (1, 2 ou 3): "))
+print("Voce escolheu:", jogos[opcao - 1])
